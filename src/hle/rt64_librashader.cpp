c9ab55970a521c02479fb9e6064417228e449873
worleydl <danworley@gmail.com>
2026-01-20 12:16:56 -0500

    librashader: wip integration

---
+++ src/hle/rt64_librashader.cpp
@@ -0,0 +1,169 @@
+//
+// RT64
+//
+
+#include "rt64_librashader.h"
+
+#include <vector>
+
+
+#ifdef _WIN32
+#include "plume_d3d12.h"
+#define LIBRA_RUNTIME_D3D12 1
+#endif
+
+#include "plume_vulkan.h"
+#define LIBRA_RUNTIME_VULKAN 1
+
+// todo: metal
+
+#include "librashader_ld.h"
+
+namespace RT64 {
+    // Librashader
+    libra_instance_t libra;
+    libra_shader_preset_t preset = nullptr;
+
+#if LIBRA_RUNTIME_D3D12
+    libra_d3d12_filter_chain_t dx_filterChain = nullptr;
+#endif
+
+#if LIBRA_RUNTIME_VULKAN
+    libra_vk_filter_chain_t vk_filterChain = nullptr;
+#endif
+
+    std::vector<LibraRuntimeParam> currentRuntimeParams;
+    std::string currentShaderPath;
+
+    Librashader::Librashader() {
+        libra = librashader_load_instance();
+    }
+
+    Librashader::~Librashader() { 
+        // todo: cleanup of libra instance?
+    }
+
+    std::string Librashader::getCurrentShader() {
+        return currentShaderPath;
+    }
+
+    std::vector<LibraRuntimeParam> Librashader::getRuntimeParams() {
+        return currentRuntimeParams;
+    }
+
+    void Librashader::updateRuntimeParam(LibraRuntimeParam parameter) {
+        // todo: api specific routing
+        if (vk_filterChain) {
+            libra.vk_filter_chain_set_param(&vk_filterChain, parameter.name.c_str(), parameter.current_value);
+        }
+    }
+
+    void Librashader::postprocess(const Librashader::LibraFrameParams &lp) {
+        // librashader requires commandlist to be wrapped for current phase
+        lp.commandList->end();
+
+        const RenderCommandList *constCmd = lp.worker->commandList.get();
+		lp.worker->commandQueue->executeCommandLists(&constCmd, 1, nullptr, 0, nullptr, 0, lp.worker->commandFence.get());
+		lp.worker->wait();
+
+        // new list for menu to avoid libra corruption
+		lp.commandList->begin();
+
+		lp.commandList->barriers(RenderBarrierStage::GRAPHICS, RenderTextureBarrier(lp.intermediateTexture, RenderTextureLayout::SHADER_READ));
+		lp.commandList->barriers(RenderBarrierStage::GRAPHICS, RenderTextureBarrier(lp.swapchainTexture, RenderTextureLayout::COLOR_WRITE));
+		lp.commandList->setFramebuffer(lp.swapchainFramebuffer);
+		lp.commandList->clearColor();
+
+		// librashader hookup
+
+        // todo: bring back dx
+        /*
+		auto* d3d12CmdList = static_cast<plume::D3D12CommandList*>(lp.commandList)->d3d;
+		auto* d3d12Input = static_cast<plume::D3D12Texture*>(lp.intermediateTexture)->d3d;
+		auto* d3d12Output = static_cast<plume::D3D12Texture*>(lp.swapchainTexture)->d3d;
+
+		libra_image_d3d12_handle_t input_handle = { d3d12Input };
+		libra_image_d3d12_handle_t output_handle = { d3d12Output };
+		libra_image_d3d12_t input = {};
+		libra_image_d3d12_t output = {};
+		input.image_type = LIBRA_D3D12_IMAGE_TYPE_RESOURCE;
+		output.image_type = LIBRA_D3D12_IMAGE_TYPE_RESOURCE;
+		input.handle = input_handle;
+		output.handle = output_handle;
+
+		libra_error_t frameErr = libra.d3d12_filter_chain_frame(&dx_filterChain, d3d12CmdList, lp.frameCount,
+																input, output, NULL, NULL, NULL);
+        */
+    }
+
+    bool Librashader::ready() {
+        return vk_filterChain; 
+    }
+
+    void Librashader::reset() {
+        /*
+        if (dx_filterChain) {
+            libra.d3d12_filter_chain_free(&dx_filterChain);
+        }
+        */
+
+        if (vk_filterChain) {
+            libra.vk_filter_chain_free(&vk_filterChain);
+        }
+
+        if (preset) {
+            libra.preset_free(&preset);
+            preset = nullptr;
+        }
+
+        currentRuntimeParams.clear();
+    }
+
+    bool Librashader::setup(RenderDevice *device, std::string path) {
+        reset();
+
+        libra_error_t err = libra.preset_create(path.c_str(), &preset);
+        // todo: error checks for preset create and get runtime params
+        //libra.preset_print(&preset);
+
+        // Build out parameter vector
+        currentRuntimeParams.clear();
+        libra_preset_param_list_t preset_parameters;
+        err = libra.preset_get_runtime_params(&preset, &preset_parameters);
+
+        for (int i = 0; i < preset_parameters.length; i++) {
+            libra_preset_param_t param = preset_parameters.parameters[i];
+            currentRuntimeParams.push_back({
+                param.name,
+                param.description,
+                param.initial, // keep track of this in case we want to reset to defaults
+                param.minimum,
+                param.maximum,
+                param.step,
+                param.initial // current_value = initial during init
+            });
+        }
+        libra.preset_free_runtime_params(preset_parameters);
+
+// todo: dx/metal
+/*
+        auto* d3d12Device = static_cast<plume::D3D12Device*>(device);
+
+		const filter_chain_d3d12_opt_t filterOptions = {
+		    LIBRASHADER_CURRENT_VERSION,
+		    false, // Force use of hlsl
+		    false, // Force disable mipmaps
+		    true   // Disable cache for UWP, it blows up the driver
+	    };
+
+        err = libra.d3d12_filter_chain_create(&preset, d3d12Device->d3d,
+                                                            &filterOptions,
+                                                            &filterChain);
+*/
+
+        if (!err)
+            currentShaderPath = path;
+
+        return !err;
+    }
+}
---
+++ src/hle/rt64_librashader.h
@@ -0,0 +1,42 @@
+//
+// RT64
+//
+
+#pragma once
+
+#include "common/rt64_plume.h"
+#include "render/rt64_render_worker.h"
+
+namespace RT64 {
+    // Based off libra preset param but also keeps track of current value alongside initial to support minimal reset to default
+    struct LibraRuntimeParam {
+        std::string name;
+        std::string description;
+        float initial;
+        float min;
+        float max;
+        float step;
+        float current_value;
+    };
+
+    struct Librashader {
+        struct LibraFrameParams {
+            RenderCommandList *commandList = nullptr;
+            RenderFramebuffer* swapchainFramebuffer = nullptr;
+            RenderTexture *intermediateTexture = nullptr;
+            RenderTexture *swapchainTexture = nullptr;
+            RenderWorker *worker = nullptr;
+            size_t frameCount;
+        };
+
+        Librashader();
+        ~Librashader();
+        std::string getCurrentShader();
+        std::vector<LibraRuntimeParam> getRuntimeParams();
+        void updateRuntimeParam(const LibraRuntimeParam param);
+        bool ready();
+        void reset();
+        bool setup(RenderDevice* device, std::string path);
+        void postprocess(const LibraFrameParams &p);
+    };
+}
